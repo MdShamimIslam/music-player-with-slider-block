e032a9197e39d71b91f9ed0d0c5b2c25463b9d64
MdShamimIslam <shamim401897@gmail.com>
2024-08-11 09:56:26 +0600

    added margin for music player progress

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '178a95d07503e6cf258a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '0de7a6561d99b6e222f7');
